2b0e3a942fd873ff8a6066a670a22304e8313b5b
saraschroeder <sara.schroeder00@gmail.com>
2023-02-19 13:11:06 -0600

    added code into index.js

---
+++ Develop/index.js
@@ -0,0 +1,193 @@
+// TODO: Include packages needed for this application
+const generateMarkdown = require('./utils/generateMarkdown');
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your application?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter the title.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'What is your GitHub Username?',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub Username.');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your email address.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'what',
+        message: 'What is your application and what problems will it solve?',
+        validate: whatInput => {
+            if (whatInput) {
+                return true;
+            } else {
+                console.log('Please enter what your application is.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'why',
+        message: 'Why did you create this application?',
+        validate: whyInput => {
+            if (whyInput) {
+                return true;
+            } else {
+                console.log('Please enter why you created this application.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'how',
+        message: 'How does someone use your application?',
+        validate: howInput => {
+            if(howInput) {
+                return true;
+            } else {
+                console.log('Please enter how to use your application.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide instructions as to how to run your application.',
+        validate: installInput => {
+            if (installInput) {
+                return true;
+            } else {
+                console.log('Please enter installation instructions.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name:'usage',
+        message: 'Please provide examples for use of application.',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('Please enter usage instructions.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Which license will you use for your application?',
+        choices: ['mit', 'agpl', 'apache', 'no license']
+    },
+    {
+        type: 'confirm',
+        name: 'contribute',
+        message: 'Please provide guidelines for contribution.',
+        when: ({ confirmContributers }) => {
+            if (confirmContributers) {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        validate: contributerInput => {
+            if (contributerInput) {
+                return true;
+            } else {
+                console.log('Please enter contributer guidelines.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Please provide instructions to test the application.',
+        validate: testInput => {
+            if (testInput) {
+                return true;
+            } else {
+                console.log('Please enter usage test instructions.');
+                return false;
+            }
+        }
+    }
+];
+ 
+
+// TODO: Create a function to write README file
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/generated-README.md', fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'File created.'
+            });
+        });
+    });
+};
+
+// TODO: Create a function to initialize app
+const init = () => {
+    return inquirer.prompt(questions)
+    .then(readmeData => {
+        return readmeData;
+    })
+}
+
+// Function call to initialize app
+init()
+.then(readmeData => {
+    console.log(readmeData);
+    return generateMarkdown(readmeData);
+})
+.then(pageMD => {
+    return writeFile(pageMD);
+})
+.then(writeFileResponse => {
+    console.log(writeFileResponse.message);
+})
+.catch(err => {
+    console.log(err);
+})
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Professional-README-Generator-1
+
+## Description
+
+In this project I created a README application for users to simply input a description, installation directions, usage, how to report issues and contributions. 
+
+## Table of Contents
+
+N/A
+
+## Installation
+
+Click on the respository link below. Clone the repository to your computer, open the file in VS Code and make sure you are in the correct directory. 
+
+Ex: Professional-README-Generator-1
+
+## Usage
+
+In order to use this application you can easily generate this README just by the command line with "node index.js", answer the questions, and your README will be automatically generated for you.
+
+## Credits
+
+N/A
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
